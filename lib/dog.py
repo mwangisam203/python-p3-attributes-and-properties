21214c474447418edf86139c038a9513de0d00db
mwangisam203 <mwangisam203@gmail.com>
2023-12-09 02:38:12 +0300

    adjusted dog.py and person.py

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,30 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    approved_breeds = ["Labrador Retriever", "German Shepherd", "Golden Retriever", "French Bulldog"]
+
+    def __init__(self, name="", breed=""):
+        self._name = ""
+        self._breed = ""
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be a string between 1 and 25 characters.")
+        else:
+            self._name = value
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value not in Dog.approved_breeds:
+            print("Breed must be in the list of approved breeds.")
+        else:
+            self._breed = value
--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,30 @@
-#!/usr/bin/env python3
-
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
 class Person:
-    pass
+    approved_jobs = ["Engineer", "Doctor", "Teacher", "Software Developer"]
+
+    def __init__(self, name="", job=""):
+        self._name = ""
+        self._job = ""
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be a string under 25 characters.")
+        else:
+            self._name = value.title()
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value not in Person.approved_jobs:
+            print("Job must be in the list of approved jobs.")
+        else:
+            self._job = value
